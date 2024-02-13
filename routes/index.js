0ede349223d72f3d00891d9b56af41b8f40d150e
Noel Johansson <noel.johansson@elev.ga.ntig.se>
2024-02-13 11:16:06 +0100

    json

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
+require('dotenv').config()
 const express = require('express')
 const router = express.Router()
 const pool = require('../db')
+const nunjucks = require('nunjucks')
+
 
 router.get('/', function (req, res) {
     res.render('index.njk')
 
-  })
-  router.get('/', function (req, res) {
+
   })
   
-  router.get('/dbtest/:id', async (req, res) => {
-    const id = req.params.id
+  router.get('/dbtest', async function(req, res){  
+    
+   const [pasta] = await pool.promise().query('SELECT * FROM noel_pasta')
 
+return res.json({pasta})
   })
   
   module.exports = router
